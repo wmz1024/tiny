e3c332e259ee1cbe6622cc57d8e8a5941e3eb463
Wmz1024 <i@wmza.cn>
2022-04-08 22:39:38 +0800

    a

--- assets/js/liny.js
+++ assets/js/liny.js
@@ -1,7 +1,7 @@
 // Big Money-nw from http://patorjk.com/software/taag/ 
 console.log("%c  ____  __    __   ____  _      ____  ____   __ __ \n /    ||  |__|  | /    || |    |    ||    \ |  |  |\n|  o  ||  |  |  ||  o  || |     |  | |  _  ||  |  |\n|     ||  |  |  ||     || |___  |  | |  |  ||  ~  |\n|  _  ||  `  '  ||  _  ||     | |  | |  |  ||___, |\n|  |  | \      / |  |  ||     | |  | |  |  ||     |\n|__|__|  \_/\_/  |__|__||_____||____||__|__||____/ \n",'color:#0f0;');
-// 拒绝任何错误控制台输出（liny网站就要干干净净的呢[doge]）
-var logDebug = false;
+// 拒绝任何错误控制台输出（liny网站就要干干净净的呢[doge] true代表显示）
+var logDebug = true;
 console.log = (function (oriLogFunc) {
   return function () {
     if (logDebug) {
