d5d65f4fc0030583a16c2e009659d6a1b0221339
Wmz1024 <i@wmza.cn>
2022-04-09 08:31:27 +0800

    a

--- assets/js/liny.js
+++ assets/js/liny.js
@@ -1,14 +1,6 @@
-// Big Money-nw from http://patorjk.com/software/taag/ 
+// from http://patorjk.com/software/taag/ 
 console.log("%c  ____  __    __   ____  _      ____  ____   __ __ \n /    ||  |__|  | /    || |    |    ||    \ |  |  |\n|  o  ||  |  |  ||  o  || |     |  | |  _  ||  |  |\n|     ||  |  |  ||     || |___  |  | |  |  ||  ~  |\n|  _  ||  `  '  ||  _  ||     | |  | |  |  ||___, |\n|  |  | \      / |  |  ||     | |  | |  |  ||     |\n|__|__|  \_/\_/  |__|__||_____||____||__|__||____/ \n",'color:#0f0;');
-// 拒绝任何错误控制台输出（liny网站就要干干净净的呢[doge] true代表显示）
-var logDebug = true;
-console.log = (function (oriLogFunc) {
-  return function () {
-    if (logDebug) {
-      oriLogFunc.apply(this, arguments);
-    }
-  }
-})(console.log);
+
 
 
 window.addEventListener('load', async () => {
@@ -38,3 +30,13 @@ self.addEventListener('install', async function (installEvent) {
             })
     );
 });
+
+self.addEventListener('fetch', async event => {
+    event.respondWith(handle(event.request))
+});
+
+const handle = async(req)=>{
+    //do something
+}
+
+
