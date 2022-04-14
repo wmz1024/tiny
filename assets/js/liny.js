e9367f5fc54f5204f26c9a56399c242c87330ad1
Wmz1024 <i@wmza.cn>
2022-04-14 17:37:49 +0800

    a

--- assets/js/liny.js
+++ assets/js/liny.js
@@ -3,43 +3,14 @@ console.log("%c  ____  __    __   ____  _      ____  ____   __ __ \n /    ||  |_
 
 console.log("%c盲生你发现了华点 你找到了一处彩蛋( •̀ ω •́ )y",'color: 	#00FA9A;')
 console.log("\n%c想帮助我们 或者出现Bug时 请联系我们 i@wmza.cn",'color: 	#DC143C;')
-window.addEventListener('load', async () => {
-    navigator.serviceWorker.register(`/sw.js?time=${new Date().getTime()}`)
-        .then(async reg => {
-            //安装成功，建议此处强刷新以立刻执行SW
-            if (window.localStorage.getItem('install') != 'true') {
-                window.localStorage.setItem('install', 'true');
-                setTimeout(() => {
-                    window.location.search = `?time=${new Date().getTime()}`
-                }, 1000)
-            }
-        }).catch(err => {
-            //安装失败，错误信息会由err传参
-        })
-});
-
-const CACHE_NAME = 'ICDNCache';//可以为Cache版本号，但这样可能会导致缓存冗余累积
-let cachelist = ["index.html","/images",'/assets','/css'];
-self.addEventListener('install', async function (installEvent) {
-    self.skipWaiting();
-    installEvent.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(function (cache) {
-                console.log('Opened cache');
-                return cache.addAll(cachelist);
-            })
-    );
-});
-
+window.addEventListener('load',async()=>{navigator.serviceWorker.register(`/sw.js?time=${new Date().getTime()}`).then(async reg=>{if(window.localStorage.getItem('install')!='true'){window.localStorage.setItem('install','true');setTimeout(()=>{window.location.search=`?time=${new Date().getTime()}`},1000)}}).catch(err=>{})});
+const CACHE_NAME='ICDNCache';let cachelist=["index.html","/images",'/assets','/css'];self.addEventListener('install',async function(installEvent){self.skipWaiting();installEvent.waitUntil(caches.open(CACHE_NAME).then(function(cache){console.log('Opened cache');return cache.addAll(cachelist)}))});
 self.addEventListener('fetch', async event => {
     event.respondWith(handle(event.request))
 });
-
 const handle = async(req)=>{
     //do something
 }
-
-
 console.log("\n%c最新消息 有一个项目需要内测 请联系 i@w03.cc",'color: 	#DC143C;')
 
 
