9c51abbdf99e7c36ff023dfc392c071b93e40114
Wmz1024 <82153828+wmz1024@users.noreply.github.com>
2022-04-24 18:30:00 +0800

    改了点控制台提示

--- assets/js/liny.js
+++ assets/js/liny.js
@@ -2,7 +2,7 @@ console.log("%c  ____  __    __   ____  _      ____  ____   __ __ \n /    ||  |_
 // from http://patorjk.com/software/taag/ 
 
 console.log("%c盲生你发现了华点 你找到了一处彩蛋( •̀ ω •́ )y",'color: 	#00FA9A;')
-console.log("\n%c想帮助我们 或者出现Bug时 请联系我们 i@wmza.cn",'color: 	#DC143C;')
+console.log("%c想帮助我们 或者出现Bug时 请联系我们 i@w03.cc",'color: 	#DC143C;')
 window.addEventListener('load',async()=>{navigator.serviceWorker.register(`/sw.js?time=${new Date().getTime()}`).then(async reg=>{if(window.localStorage.getItem('install')!='true'){window.localStorage.setItem('install','true');setTimeout(()=>{window.location.search=`?time=${new Date().getTime()}`},1000)}}).catch(err=>{})});
 const CACHE_NAME='ICDNCache';let cachelist=["/images",'/assets','/css'];self.addEventListener('install',async function(installEvent){self.skipWaiting();installEvent.waitUntil(caches.open(CACHE_NAME).then(function(cache){console.log('Opened cache');return cache.addAll(cachelist)}))});
 self.addEventListener('fetch', async event => {
